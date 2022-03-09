ea1496b775c950087aac7a03dced2103792fdd0d
Ant0nRocket <anton.stephanyak@gmail.com>
2022-03-10 02:55:46 +0500

    [feat] Deserialization by string and Type [feat] Version 1.5.6

--- Ant0nRocket.Lib.Std20/IO/DefaultSerializer.cs
+++ Ant0nRocket.Lib.Std20/IO/DefaultSerializer.cs
@@ -1,10 +1,17 @@
-﻿namespace Ant0nRocket.Lib.Std20.IO
+﻿using System;
+
+namespace Ant0nRocket.Lib.Std20.IO
 {
     internal class DefaultSerializer : ISerializer
     {
         public T Deserialize<T>(string contents)
         {
             return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(contents);
+        }
+
+        public object Deserialize(string contents, Type type)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject(contents, type);
         }
 
         public string Serialize(object obj)
--- Ant0nRocket.Lib.Std20/IO/ISerializer.cs
+++ Ant0nRocket.Lib.Std20/IO/ISerializer.cs
@@ -1,8 +1,12 @@
-﻿namespace Ant0nRocket.Lib.Std20.IO
+﻿using System;
+
+namespace Ant0nRocket.Lib.Std20.IO
 {
     public interface ISerializer
     {
         T Deserialize<T>(string contents);
+
+        object Deserialize(string contents, Type type);
 
         string Serialize(object obj);
     }

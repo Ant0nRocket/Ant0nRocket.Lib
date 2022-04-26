98e20daeaee4c6f3c766e9c99e01db1ca480601a
Ant0nRocket <anton.stephanyak@gmail.com>
2022-04-26 23:51:08 +0500

    [feat] Version 1.6.4 [feat] GetTypesAccrossAppDomainWithAttribute

--- Ant0nRocket.Lib.Std20/Reflection/AttributeUtils.cs
+++ Ant0nRocket.Lib.Std20/Reflection/AttributeUtils.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Ant0nRocket.Lib.Std20.Reflection
 {
@@ -6,5 +8,37 @@ namespace Ant0nRocket.Lib.Std20.Reflection
     {
         public static T GetAttribute<T>(Type type) where T : Attribute =>
             (T)Attribute.GetCustomAttribute(type, typeof(T));
-    }
+
+        public static Type GetTypeAccrossAppDomain(string typeName)
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (var assembly in assemblies)
+            {
+                var types = assembly.GetTypes();
+                var targetType = types.Where(t => t.FullName == typeName).FirstOrDefault();
+                if (targetType != null)
+                    return targetType;
+            }
+
+            return null;
+        }
+
+        public static List<Type> GetTypesAccrossAppDomainWithAttribute<T>() where T : Attribute
+        {
+            var result = new List<Type>();
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (var assembly in assemblies)
+            {
+                var types = assembly.GetTypes();
+                foreach (var type in types)
+                {
+                    var targetAttribute = GetAttribute<T>(type);
+                    if (targetAttribute != default)
+                        result.Add(type);
+                }
+            }
+
+            return result;
+        }
+    }    
 }

3f2260e9b9919eeda50a759a7a683961fb11e34b
Ant0nRocket <9224340306@ya.ru>
2022-11-01 15:15:58 +0500

    [feat] StartOfTheDay extension [v] 2.1.0-preview4

--- Ant0nRocket.Lib.Std20/Extensions/DateTimeExtension.cs
+++ Ant0nRocket.Lib.Std20/Extensions/DateTimeExtension.cs
@@ -5,6 +5,17 @@ namespace Ant0nRocket.Lib.Std20.Extensions
     public static class DateTimeExtension
     {
         /// <summary>
+        /// 20.01.2021 12:45:00 -> 20.01.2021 00:00:00.000
+        /// </summary>
+        public static DateTime StartOfTheDay(this DateTime date)
+        {
+            var year = date.Year;
+            var month = date.Month;
+            var day = date.Day;
+            return new DateTime(year, month, day, 0, 0, 0, 0);
+        }
+
+        /// <summary>
         /// 20.01.2021 12:45:00 -> 20.01.2021 23:59:59.999
         /// </summary>
         public static DateTime EndOfTheDay(this DateTime date)
